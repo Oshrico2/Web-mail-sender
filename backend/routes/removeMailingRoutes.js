2ca3f0895085d042a1e4df9b990a15fb2e2f0671
osher <oshrico2@gmail.com>
2024-07-10 12:27:47 +0300

    add remove mailing to history

--- backend/routes/removeMailingRoutes.js
+++ backend/routes/removeMailingRoutes.js
@@ -1,22 +1,41 @@
 import express from "express";
-import {fetchAgents} from '../utils/scripts.js'
-
+import { fetchAgents } from "../utils/scripts.js";
+import UserActivity from "../models/userActivityModel.js";
+import {
+  getCurrentDateFormatted,
+  getCurrentTimeFormatted,
+} from "../utils/scripts.js";
 
 const router = express.Router();
 
-router.put('/', async (req,res) => {
-    const {email} = req.body;
-    console.log(email);
-    const agents = await fetchAgents();
-    const agent = agents.find(agent => agent.email === email);
-    if (agent){
-        agent.confirmedMailing = false;
-        await agent.save()
-        res.status(200).send('Successfully saved agent details')
-    }else{
-        res.status(404);
-        throw new Error("Agent not found");
-    }
-})
+router.put("/", async (req, res) => {
+  const { email } = req.body;
+
+  console.log(email);
+  const agents = await fetchAgents();
+  const agent = agents.find((agent) => agent.email === email);
+  const date = Date.now()
+  if (agent) {
+    agent.confirmedMailing = false;
+    await agent.save();
+    const activity = new UserActivity({
+        action: "הסיר את עצמו מרשימת תפוצה",
+        title: "הסרה מרשימת תפוצה",
+        createdBy: "undefined",
+        entityName: agent.name,
+        description: `${agent.name} - הסיר את עצמו מרשימת תפוצה 
+       בתאריך ${getCurrentDateFormatted(date)} בשעה ${getCurrentTimeFormatted(
+          date
+        )}`,
+        color: 'orange',
+        createdAt: date,
+      });
+      await activity.save();
+    res.status(200).send("Successfully saved agent details");
+  } else {
+    res.status(404);
+    throw new Error("Agent not found");
+  }
+});
 
 export default router;
